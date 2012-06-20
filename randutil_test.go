6f0b985a7c16b9a658841978e6e915b3fb71187c
Jason McVetta <jason.mcvetta@gmail.com>
2012-06-19 17:31:10 -0700

    Update license

--- randutil_test.go
+++ randutil_test.go
@@ -1,27 +1,5 @@
-/*
-                                    Goutil
-                                  Test Suite
-
-
-@author: Jason McVetta <jason.mcvetta@gmail.com>
-@copyright: (c) 2012 Jason McVetta
-@license: GPL v3 - http://www.gnu.org/copyleft/gpl.html
-
-********************************************************************************
-Randutil is free software: you can redistribute it and/or modify it under the
-terms of the GNU General Public License as published by the Free Software
-Foundation, either version 3 of the License, or (at your option) any later
-version.
-
-Randutil is distributed in the hope that it will be useful, but WITHOUT ANY
-WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
-PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License along with
-Goutil.  If not, see <http://www.gnu.org/licenses/>.
-********************************************************************************
-
-*/
+// Copyright (c) 2012 Jason McVetta.  This is Free Software, released under the 
+// terms of the GPL v3.  See http://www.gnu.org/copyleft/gpl.html for details.
 
 package randutil
 
